a1a29d26c391676d4958c99fb61432d824073c6e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-05 19:04:18 +0200

    django settings

--- tweetme/settings/production.py
+++ tweetme/settings/production.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2$w#ua6kln)29iwwe($k+y8#=0!x(*x-%xqup%7j&=w+idlguf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
