debfe17d584ae91ebc40ee9f261a38fd6852303e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-05 20:44:55 +0200

    start app tweets

--- tweets/urls.py
+++ tweets/urls.py
@@ -14,16 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from django.conf import settings
-from django.conf.urls.static import static
+from .views import home
+
+app_name = 'tweets'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('tweets.urls', namespace='tweets')),
+  path('', home, name='home'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
