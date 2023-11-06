972086032e0cc643b4e9d598aa15e8871bf2e222
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-06 12:24:14 +0200

    relocate dirs

--- tweetme/settings/local.py
+++ tweetme/settings/local.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,7 +128,6 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = [
     BASE_DIR / 'media',
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
