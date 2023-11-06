a1ed1a6fc0331c5b1ed2ec0cafa1b8dbfff6ecf3
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-06 10:09:42 +0200

    tweets model

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
+class Tweet(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  content = models.CharField(max_length=250, blank=True, null=True)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return str(self.content)
