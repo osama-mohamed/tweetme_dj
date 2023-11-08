19592af50f9bb0295ec183bfcc5138a7bb050b6e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-08 19:29:41 +0200

    js tweets list, search

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,16 +1,34 @@
 from rest_framework.serializers import ModelSerializer
+from django.urls import reverse_lazy, reverse
 
-from accounts.api.serializers import UserDisplaySerializers
+
+from accounts.api.serializers import UserDisplaySerializers, SerializerMethodField
 from tweets.models import Tweet
 
 
 class TweetModelSerializer(ModelSerializer):
   user = UserDisplaySerializers()
+  url = SerializerMethodField()
+  update_url = SerializerMethodField()
+  delete_url = SerializerMethodField()
+
   class Meta:
     model = Tweet
     fields = [
       'user',
+      'id',
       'content',
       'timestamp',
-      'id',
+      'url',
+      'update_url',
+      'delete_url',
     ]
+
+  def get_url(self, obj):
+    return self.context.get('request').build_absolute_uri(obj.get_absolute_url())
+  
+  def get_update_url(self, obj):
+    return self.context.get('request').build_absolute_uri(reverse('tweets:update', kwargs={'pk': obj.pk}))
+  
+  def get_delete_url(self, obj):
+    return self.context.get('request').build_absolute_uri(reverse('tweets:delete', kwargs={'pk': obj.pk}))
--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,11 +1,23 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
+from django.db.models import Q
+
 
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 
 
 class TweetListAPIView(ListAPIView):
-  queryset = Tweet.objects.all()
+  # queryset = Tweet.objects.all()
   serializer_class = TweetModelSerializer
   permission_classes = [AllowAny]
+
+  def get_queryset(self, *args, **kwargs):
+    qs = Tweet.objects.all()
+    query = self.request.GET.get('q', None)
+    if query is not None:
+      qs = qs.filter(
+        Q(content__icontains=query) |
+        Q(user__username__icontains=query)
+      )
+    return qs
