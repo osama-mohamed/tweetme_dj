981be5530085dc556facc22f21ec4d22ecdab34e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-09 20:59:19 +0200

    user logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from .views import (
   UserDetailView,
@@ -7,5 +8,6 @@ from .views import (
 app_name = 'accounts'
 
 urlpatterns = [
+  path('logout/', LogoutView.as_view(), name='logout'),
   path('<str:username>/', UserDetailView.as_view(), name='detail'),
 ]
