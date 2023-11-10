5e2993a49f594030d1fa45f97ecbd2aee9a2bd06
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-10 09:24:26 +0200

    user profile, followers

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile', on_delete=models.CASCADE) # user.profile
+  following = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followed_by', blank=True)
+  # user.profile.following -- users I follow
+  # user.followed_by -- users that follow me - reverse relationship
+
+  def __str__(self):
+    return str(self.following.all().count())
