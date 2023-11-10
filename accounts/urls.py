7ccba9a2e4301c00c80f0c6538bc06383c1fc8d6
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-10 10:05:58 +0200

    toggle follow view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,13 @@ from django.contrib.auth.views import LogoutView
 
 from .views import (
   UserDetailView,
+  UserFollowView,
 )
 
 app_name = 'accounts'
 
 urlpatterns = [
   path('logout/', LogoutView.as_view(), name='logout'),
+  path('<str:username>/follow/', UserFollowView.as_view(), name='follow'),
   path('<str:username>/', UserDetailView.as_view(), name='detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.contrib.auth import get_user_model
+from django.views import View
 from django.views.generic import DetailView
+
+from .models import UserProfile
 
 
 User = get_user_model()
@@ -19,3 +22,14 @@ class UserDetailView(DetailView):
     context['title'] = 'User Detail'
     return context
 
+
+class UserFollowView(View):
+  def get(self, request, username, *args, **kwargs):
+    toggle_user = get_object_or_404(User, username__exact=username)
+    if request.user.is_authenticated:
+      user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+      if toggle_user in user_profile.following.all():
+        user_profile.following.remove(toggle_user)
+      else:
+        user_profile.following.add(toggle_user)
+    return redirect('accounts:detail', username=username)
