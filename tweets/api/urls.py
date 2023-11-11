b3e2533e8430ad9be81181140a8ccafd59d8eba7
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-11 21:37:56 +0200

    retweet by js

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
   TweetListAPIView,
   TweetCreateAPIView,
+  RetweetAPIView,
 )
 
 app_name = 'tweets_api'
@@ -10,4 +11,5 @@ app_name = 'tweets_api'
 urlpatterns = [
   path('', TweetListAPIView.as_view(), name='list'),
   path('create/', TweetCreateAPIView.as_view(), name='create'),
+  path('<int:pk>/retweet/', RetweetAPIView.as_view(), name='retweet'),
 ]
