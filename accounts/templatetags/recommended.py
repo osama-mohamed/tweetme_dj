e3c3b13463706277693f8ee2e3f0c434e94eeebd
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2023-11-16 19:50:40 +0200

    recommended users simple tags another way

--- accounts/templatetags/recommended.py
+++ accounts/templatetags/recommended.py
@@ -12,3 +12,25 @@ def recommended(user):
   if isinstance(user, User):
     qs = UserProfile.objects.recommended(user)
     return {'recommended': qs}
+
+
+
+# from django.contrib.auth import get_user_model
+# from django import template
+# from django.template.loader import get_template
+# from django.utils.safestring import mark_safe
+
+# from accounts.models import UserProfile
+
+# register = template.Library()
+# User = get_user_model()
+
+# @register.simple_tag # @register.simple_tag(is_safe=True)
+# def recommended(user):
+#   if isinstance(user, User):
+#     template = get_template('accounts/snippets/recommended.html')
+#     context = {'recommended': UserProfile.objects.recommended(user)}
+#     data = template.render(context=context, request=None)
+#     content = mark_safe(data) # @register.filter(is_safe=True)
+#     return content
+#   return ''
